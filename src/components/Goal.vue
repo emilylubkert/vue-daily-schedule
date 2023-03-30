fc3dc4a03bff431d87481a0f0ddb45dc7329d1de
Emily Lubkert <elubkert@gmail.com>
2023-03-30 13:09:38 -0600

    update Goal

--- src/components/Goal.vue
+++ src/components/Goal.vue
@@ -1,19 +1,40 @@
 <script>
+let id = 0;
+
 export default {
     data() {
-        goal: ''
+        return{
+            newGoal: '',
+            goals: []
+        }
+    },
+    methods: {
+        addGoal() {
+            this.goals.push({id: id++, text: this.newGoal, done: false})
+            this.newGoal = '';
+        }
     }
-
 }
 
 
 
 </script>
 
+
 <template>
     <div id="container">
-        <h3>GOAL FOR THE DAY</h3>
-        <input v-model="goal" />
+
+    <form @submit.prevent="addGoal">
+        <h3>GOALS FOR THE DAY</h3>
+        <input type="text" v-model="newGoal" />
+        <button>Add Goal</button>
+    </form>
+    <ul>
+        <li v-for="goal in goals" :key="goal.id">
+            <input type="checkbox" v-model="goal.done">
+            <span :class="{ done: goal.done }">{{ goal.text }}</span>
+        </li>
+    </ul>
     </div>
 </template>
 
@@ -22,8 +43,17 @@ h3 {
     padding: 0 1em;
 }
 
-input {
+input[type=text] {
     width: 100%;
+}
+
+li {
+    list-style-type: none;
+    display: flex;
+}
+
+.done {
+  text-decoration: line-through;
 }
 
 
