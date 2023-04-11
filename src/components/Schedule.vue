d7ceade9fd016cbd098c84d4d3d173e5250bc768
Emily Lubkert <elubkert@gmail.com>
2023-04-11 16:14:17 -0600

    add schedule

---
+++ src/components/Schedule.vue
@@ -0,0 +1,67 @@
+<script>
+import _ from 'lodash';
+let id = 0;
+
+
+
+export default {
+    data() {
+        return{
+            newAppt: '',
+            appts: [],
+            times: _.range(7, 13).concat(_.range(1, 9))
+        }
+    },
+    methods: {
+        addAppt() {
+            this.appts.push({id: id++, text: this.newAppt})
+            this.newAppt = '';
+
+
+        },
+        clear() {
+            this.appts = [];
+        }
+    }
+}
+
+
+
+
+
+</script>
+
+
+<template>
+    <ul>
+        <li v-for="time in times" :key="time">
+            <label >{{ time }}</label>
+            <input type="text" />
+        </li>
+    </ul>
+
+</template>
+
+<style scoped>
+h3 {
+    padding: 0 1em;
+}
+
+input[type=text] {
+    width: 100%;
+}
+
+li {
+    list-style-type: none;
+    display: flex;
+}
+
+.done {
+  text-decoration: line-through;
+}
+
+
+#container {
+    display: flex;
+}
+</style>
